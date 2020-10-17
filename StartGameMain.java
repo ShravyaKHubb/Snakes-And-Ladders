abca5b30b099e8ad91ae0be84bdf8eb05c57cbae
ShravyaHub <72798726+ShravyaHub@users.noreply.github.com>
2020-10-17 09:25:44 +0530

    Update StartGameMain.java

--- StartGameMain.java
+++ StartGameMain.java
@@ -5,14 +5,9 @@ public class StartGameMain {
     public static void main(String[] args) {
 
         int playerPosition = 1, dieRoll;
-
-        while(playerPosition != 100) {
-
-            Random random = new Random();
-            dieRoll = random.nextInt(6) + 1;
-
-        }
-
+        Random random = new Random();
+        dieRoll = random.nextInt(6) + 1;
+        
     }
 
 }
