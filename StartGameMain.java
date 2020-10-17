3db866b5910c935feb5720730e3a126eefde2a77
ShravyaHub <shravyak514@gmail.com>
2020-10-17 09:10:34 +0530

    Refactored file: StartGameMain.java. Game checks for no play, ladder or snake using Random method

---
+++ StartGameMain.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+public class StartGameMain {
+
+    public static final int START_POSITION = 0;
+    public static final int WINNING_POSITION = 100;
+    public static final int NO_PLAY = 1;
+    public static final int LADDER = 2;
+    public static final int SNAKE = 3;
+
+    public static void main(String[] args) {
+
+        int playerPosition = START_POSITION, dieRoll, options;
+        Random random = new Random();
+        dieRoll = random.nextInt(6) + 1;
+        options = random.nextInt(3) + 1;
+
+            if(options == NO_PLAY) {
+
+                playerPosition += 0;
+
+            } else if (options == LADDER) {
+
+                playerPosition += dieRoll;
+                System.out.println("Player landed on a ladder");
+
+            } else if (options == SNAKE){
+
+                playerPosition -= dieRoll;
+                System.out.println("Player landed on a snake");
+
+            }
+
+            System.out.println("Player positon = " + playerPosition);
+
+    }
+
+}
