df8adfa2909b75407fadb28505f80ea91f13e8f1
ShravyaHub <72798726+ShravyaHub@users.noreply.github.com>
2020-10-17 08:47:53 +0530

    Update StartGameMain.java

--- StartGameMain.java
+++ StartGameMain.java
@@ -2,7 +2,7 @@ public class StartGameMain {
 
     public static void main(String[] args) {
 
-        int playerPosition = 1, dieRoll;
+        int playerPosition = 1;
 
         while(playerPosition != 100) {
 
