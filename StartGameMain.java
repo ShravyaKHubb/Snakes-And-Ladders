8e3425eed68bb2e56da706d2b6c0c64516a8b8ea
ShravyaHub <shravyak514@gmail.com>
2020-10-17 08:46:17 +0530

    Added file: StartGameMain.java. The game start with player at position one

---
+++ StartGameMain.java
@@ -0,0 +1,13 @@
+public class StartGameMain {
+
+    public static void main(String[] args) {
+
+        int playerPosition = 1, dieRoll;
+
+        while(playerPosition != 100) {
+
+        }
+
+    }
+
+}
