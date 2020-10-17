fb7fb6490d683d0db71a4d3a6c5e22a640c332c6
ShravyaHub <shravyak514@gmail.com>
2020-10-17 08:51:11 +0530

    Refactored file: StartGameMain.java. Player rolls a die and get a number between one and six using Random method

---
+++ StartGameMain.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class StartGameMain {
+
+    public static void main(String[] args) {
+
+        int playerPosition = 1, dieRoll;
+
+        while(playerPosition != 100) {
+
+            Random random = new Random();
+            dieRoll = random.nextInt(6) + 1;
+
+        }
+
+    }
+
+}
